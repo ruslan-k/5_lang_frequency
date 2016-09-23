5cb4fc7a40dd8f3a45ddc6ef6172a8e0036392e7
Ruslan Kurbanov <wkurbanov@gmail.com>
2016-09-23 04:23:51 +0400

    update lang_frequency.py

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,19 @@
 
 
+from collections import Counter
+import re
+
 def load_data(filepath):
-    pass
+    f = open(filepath)
+    return f.read()
 
 
 def get_most_frequent_words(text):
-    pass
-
+    text = re.sub(r'\d', '', text)
+    text_list = [word.lower() for word in re.findall("\w+-\w+|[\w']+", text)]
+    words_dict = Counter(text_list)
+    for word, num in words_dict.most_common()[:10]:
+        print("{}: {}".format(word, num))
 
 if __name__ == '__main__':
-    pass
+    print(get_most_frequent_words(load_data('text.txt')))
