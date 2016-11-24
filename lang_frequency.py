b98830241312c42fee2de5da334c00db382ca770
Ruslan Kurbanov <wkurbanov@gmail.com>
2016-11-25 00:59:20 +0400

    Update lang_frequency.py

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,17 +1,21 @@
-
-
 from collections import Counter
 import re
 
 def load_data(filepath):
-    f = open(filepath)
-    return f.read()
+    with open(filepath) as infile:
+        return infile.read()
 
 
 def get_most_frequent_words(text):
-    text = re.sub(r'\d', '', text)
-    text_list = [word.lower() for word in re.findall("\w+-\w+|[\w']+", text)]
+    # список всех слов (выражение учитывает английский слова и кириллицу, слова не должны начинаться или заканчиваться c "-")
+    word_list = re.findall(r"[а-яА-ЯA-Za-z]+-[а-яА-ЯA-Za-z]+|[а-яА-ЯA-Za-z]+", text, re.UNICODE)
+    # список всех слов c маленькой буквы
+    text_list = [word.lower() for word in word_list]
+
+    # осталвнеие списка {слово: число (как часто встречается)}
     words_dict = Counter(text_list)
+
+    # вывод top 10
     for word, num in words_dict.most_common()[:10]:
         print("{}: {}".format(word, num))
 
