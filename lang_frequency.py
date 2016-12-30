4b42dc975c68f90232b9df98228cb6f87f73c128
Ruslan Kurbanov <wkurbanov@gmail.com>
2016-12-30 18:57:19 +0300

    Moved exception to function, renamed variables, moved constant to variable

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,31 +1,33 @@
+import os
+import re
 import sys
 from collections import Counter
-import re
+
 
 def load_data(filepath):
-    with open(filepath) as infile:
-        return infile.read()
+    if os.path.exists(filepath):
+        with open(filepath) as infile:
+            return infile.read()
+    else:
+        sys.exit("Неверный путь к файлу или имя файла")
 
 
-def get_most_frequent_words(filename):
-    text = load_data(filename)
-    all_words_in_text = re.findall(r"[а-яА-ЯA-Za-z]+-?[а-яА-ЯA-Za-z]+", text, re.UNICODE)
-    all_words_in_text_lower = [word.lower() for word in all_words_in_text]
-    counted_words = Counter(all_words_in_text_lower)
-
-    return counted_words
+def get_most_frequent_words(filename, number_or_words):
+    file_content = load_data(filename)
+    all_words_in_file = re.findall(r"[а-яА-ЯA-Za-z]+-?[а-яА-ЯA-Za-z]+", file_content, re.UNICODE)
+    all_words_in_file_lower = [word.lower() for word in all_words_in_file]
+    counted_words = Counter(all_words_in_file_lower)
+    most_common_words = counted_words.most_common()[:number_or_words]
+    return most_common_words
 
 
 def print_frequent_words(data):
-    top_10_common_words = data.most_common()[:10]
-    for word, rate in top_10_common_words:
+    for word, rate in data:
         print("{}: {}".format(word, rate))
 
 
 if __name__ == '__main__':
-    try:
-        filename = sys.argv[1]
-        frequent_words = get_most_frequent_words(filename)
-        print(print_frequent_words(frequent_words))
-    except FileNotFoundError:
-        print("Неверный путь к файлу или имя файла")
+    number_or_words_to_print = 10
+    filename = sys.argv[1]
+    frequent_words = get_most_frequent_words(filename, number_or_words_to_print)
+    print_frequent_words(frequent_words)
